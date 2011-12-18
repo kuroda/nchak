2cd30d0fdcba1920d3eacc214471bc675eb1c526
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-18 19:48:32 +0900

    Rails 3.1.3 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nchak::Application.config.secret_token = 'daa5b5fb988b2246ee00c57e3e8c3cdcbde292b502a066dc88712eb1445db7fe92cd1d14e628cc417513886050f558967511238731c40d46a5a0dea6ac3366e2'
