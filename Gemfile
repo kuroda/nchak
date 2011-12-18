4c61c715dd50dee89e1e5581cbedd9bce1362ed5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-18 20:44:18 +0900

    第12回: ユーザー認証(1)

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.3'
 
 gem 'sqlite3'
 gem 'will_paginate', '3.0.2'
+gem 'devise', '1.5.2'
 
 gem 'json'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Nchak::Application.routes.draw do
+  devise_for :users
+
+  root :to => 'welcome#index'
+  
   resources :tasks, :only => [ :index, :create ] do
     put :finish, :on => :member
     put :unfinish, :on => :member
