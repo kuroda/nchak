8b36f3f33ba5d9ee618baeea7e5d1c4391a5424e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-18 20:26:07 +0900

    第9回: タスクの完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,10 @@ class TasksController < ApplicationController
     @task.save
     redirect_to :tasks
   end
+
+  def finish
+    @task = Task.find(params[:id])
+    @task.update_attribute(:done, true)
+    redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Nchak::Application.routes.draw do
-  resources :tasks, :only => [ :index, :create ]
+  resources :tasks, :only => [ :index, :create ] do
+    put :finish, :on => :member
+  end
 end
