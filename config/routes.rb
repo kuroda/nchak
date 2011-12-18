e6e3f0aae0a9c4572869c25d9a82d6b0ebaf47f8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-18 20:59:21 +0900

    第13回: ユーザー認証(2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Nchak::Application.routes.draw do
-  devise_for :users
-
   root :to => 'welcome#index'
   
+  devise_for :users
+  get 'tasks', :to => 'tasks#index', :as => :user_root
+
   resources :tasks, :only => [ :index, :create ] do
     put :finish, :on => :member
     put :unfinish, :on => :member
